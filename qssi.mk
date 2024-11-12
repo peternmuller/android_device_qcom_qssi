8ec07dbfc7832d7f27f744712a2350f54b73f9d2
Karra Harshitha <quic_kharshit@quicinc.com>
2024-11-12 13:49:09 +0530

    Enable qspa default properties for qssi lunch
    Change-Id: I38e816fd23181814684883b062730f4a9e97ee99

--- qssi.mk
+++ qssi.mk
@@ -133,6 +133,7 @@ TARGET_USES_NQ_NFC := true
 PRODUCT_CHARACTERISTICS := nosdcard
 BOARD_FRP_PARTITION_NAME := frp
 
+PRODUCT_PACKAGES += qspa_system.rc qspa_default.rc
 #Android EGL implementation
 PRODUCT_PACKAGES += libGLES_android
 
